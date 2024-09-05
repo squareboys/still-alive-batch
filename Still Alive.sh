b72ffa30e5941c9be56b8caaa734422b6c2bc78f
squareboys <126035613+squareboys@users.noreply.github.com>
2024-09-05 17:57:14 -0400

    Add files via upload

---
+++ Still Alive.sh
@@ -0,0 +1,157 @@
+tput setaf 3
+echo "This was a triumph."
+sleep .8
+echo "I'm making a note here:"
+sleep .8
+echo "huge success."
+echo""
+sleep 1
+echo "It's hard to overstate"
+sleep .8
+echo "My satisfaction."
+echo""
+sleep 1
+echo "Aperture Science."
+sleep .8
+echo "We do what we must"
+sleep .8
+echo "Because we can."
+sleep 1
+echo "For the good of all of us."
+sleep 1
+echo "Except the ones who are dead."
+echo ""
+sleep .8
+echo "But there's no sense crying"
+sleep .8
+echo "Over every mistake."
+sleep 1
+echo "You just keep on trying"
+sleep .8
+echo "Till you run out of cake."
+sleep .8
+echo "And the Science gets done."
+sleep .8
+echo "And you make a neat gun."
+sleep .8
+echo "For the people who are"
+sleep .8
+echo "Still alive."
+echo ""
+sleep .5
+clear
+sleep .8
+echo""
+echo "I'm not even angry."
+sleep .8
+echo "I'm being so sincere right now."
+sleep .8
+echo "Even though you broke my heart."
+sleep .8
+echo "And killed me."
+echo ""
+sleep .8
+
+echo "And tore me to pieces."
+sleep .8
+echo "And threw every piece into a fire."
+sleep .8
+echo "As they burned it hurt because"
+sleep .8
+echo "I was so happy for you!"
+echo ""
+sleep .8
+echo "Now these points of data"
+sleep .8
+echo "Make a beautiful line."
+sleep .8
+echo "And we're out of beta."
+sleep .8
+echo "We're releasing on time."
+sleep .8
+echo "So I'm GLaD. I got burned."
+sleep .8
+echo "Think of all the things we learned"
+sleep .8
+echo "For the people who are"
+sleep .8
+echo "Still alive."
+echo ""
+sleep .8
+
+echo "Go ahead and leave me."
+sleep .8
+echo "I think I prefer to stay inside."
+sleep .8
+echo "Maybe you'll find someone else"
+sleep .8
+echo "To help you."
+sleep .8
+echo "Maybe Black Mesa..."
+sleep .8
+echo "THAT WAS A JOKE, HA HA, FAT CHANCE."
+echo ""
+sleep .5
+clear
+sleep .8
+
+echo "Anyway this cake is great"
+sleep .8
+echo "It's so delicious and moist"
+echo ""
+sleep .8
+
+echo "Look at me still talking when there's science to do"
+sleep .8
+echo "When I look out there"
+sleep .8
+echo "It makes me GLaD I'm not you."
+echo ""
+sleep .8
+
+echo "I've experiments to run"
+sleep .8
+echo "There is research to be done"
+sleep .8
+echo "On the people who are"
+sleep .8
+echo "Still alive."
+echo ""
+sleep .8
+echo "And believe me I am still alive"
+sleep .8
+echo "I'm doing science and I'm still alive"
+sleep .8
+echo "I feel FANTASTIC and I'm still alive"
+sleep .8
+echo "While you're dying I'll be still alive"
+sleep .8
+echo "And when you're dead I will be still alive"
+sleep .8
+echo "Still alive
+Still alive."
+sleep .8
+echo  '  
+             .,-:;//;:=,
+          . :H@@@MM@M#H/.,+%;,
+       ,/X+ +M@@M@MM%=,-%HMMM@X/,
+     -+@MM; $M@@MH+-,;XMMMM@MMMM@+-
+    ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.
+  ,>MM@@MH ,@%=            .---=-=:=,.
+  =@#@@@MX .,              -%HX$$%%%+;
+ =-./@M@M$                  .;@MMMM@MM:
+ X@/.-$MM/                    .+MM@@@M$
+,@M@H: :@:                    . =X#@@@@-
+,@@@MMX, .                    /H- ;@M@M=
+.H@@@@M@+,                    %MM+..%#$.
+ /MMMM@MMH/.                  XM@MH, =;
+  /%+%$XHH@$=              , .H@@@@MX,
+   .=--------.           -%H.,@@@@@MX,
+   .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.
+     =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=
+       =%@M@M#@$-.=$@MM@@@M: %M<=
+         ,:+$+-,/H#MMMMMMM@= =,
+               =++%%%%+/:-
+
+'
+sleep 10
